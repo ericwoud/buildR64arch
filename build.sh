033b5e99ee72034ff51b0c9487a4a0f4aa1d5cde
Eric Woudstra <ericwouds@gmail.com>
2025-04-15 15:45:19 +0200

    ubuntu add kmod package

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ IMAGE_FILE="./bpir.img"        # Name of image
 NEEDED_PACKAGES="hostapd wireless-regdb iproute2 nftables f2fs-tools dosfstools\
  btrfs-progs patch sudo evtest parted linux-firmware binutils"
 NEEDED_PACKAGES_DEBIAN="openssh-server device-tree-compiler mmc-utils\
- libpam-systemd systemd-timesyncd systemd-resolved"
+ libpam-systemd systemd-timesyncd systemd-resolved kmod"
 NEEDED_PACKAGES_ALARM=" openssh        dtc                  mmc-utils-git\
  base dbus-broker-units"
 STRAP_PACKAGES_ALARM="pacman archlinuxarm-keyring inetutils"
