c049cd73c65639cccd2424aeb389e272f287e276
Eric Woudstra <ericwouds@gmail.com>
2026-02-12 11:19:55 +0000

    set PATH fixed

--- build.sh
+++ build.sh
@@ -465,7 +465,7 @@ function ctrl_c() {
 
 function dochroot() {
   hostname "${target}"
-  set -a; source "${rootfsdir}/etc/environment"; set +a
+  export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
   chroot "${rootfsdir}" "$@"
 }
 
